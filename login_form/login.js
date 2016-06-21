ae8868c05e121113db66f76ebe72c9253523ba4e
Vito Fan <vitogalaxy@hotmail.com>
2016-06-21 09:27:44 +0800

    update

---
+++ login_form/login.js
@@ -0,0 +1,9 @@
+(function () {
+	var form = document.getElementsByTagName('form')[0];
+var userName = document.querySelector('#text_input');
+var passWord = document.querySelector('#password_input');
+
+var submit = document.getElementById('submit');
+
+alert("hello");
+}());
